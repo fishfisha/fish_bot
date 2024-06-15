b82bae8ab3098d084b362c0f4c7e15cff8daaa17
fishfisha <mashagol10@mail.ru>
2024-06-15 17:34:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,21 @@
-# fish_bot
+import discord
+from discord.ext import commands
 
-Всем привет! Это мой первый бот на классе Bot.
+intents = discord.Intents.default()
+intents.message_content = True
 
-Есть следующие команды:
-1. $hello - с вами поздоровается наш бот
-2. $heh <число> - столько раз наш бот скажет "he"
-3. $add <число> и <число> - сложение 2х чисел
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("")
